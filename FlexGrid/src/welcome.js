bfa3aacd8c44f549a2767844c607781f7bcf5fbb
gregoryagu <greg.gum@gmail.com>
2015-12-24 13:04:25 -0500

    Updates

--- FlexGrid/src/welcome.js
+++ FlexGrid/src/welcome.js
@@ -11,9 +11,69 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, Promi
         step("next", void 0);
     });
 };
+import errorHandler from "resources/core/error-handler";
+var wijmo;
 export class Welcome {
-    constructor() {
+    constructor(entityManagerProvider, element) {
+        this.entityManagerProvider = entityManagerProvider;
+        this.element = element;
         this.heading = "Welcome";
+        this.location = "FileRowMaps";
+        this.entities = [];
+        console.log("mapping-list cstr");
+        console.log(entityManagerProvider.toString());
+        console.log(element.toString());
+        entityManagerProvider.initializeEntityManager()
+            .then((em) => {
+            this.entityManager = em;
+            this.getFlexData();
+        });
+    }
+    getInitialData() {
+        return __awaiter(this, void 0, Promise, function* () {
+            let query = new breeze.EntityQuery()
+                .from('FileRowMaps');
+            try {
+                let queryResult = yield this.entityManager.executeQuery(query);
+                this.entities = queryResult.results;
+            }
+            catch (error) {
+                errorHandler.handleError(error, this.location);
+            }
+        });
+    }
+    attached() {
+        console.log("attached");
+        let flex = new wijmo.grid.FlexGrid(this.element);
+        flex.initialize({
+            itemsSource: this.items
+        });
+    }
+    getFlexData() {
+        this.items = new wijmo.data.BreezeCollectionView(this.entityManager, this.entityManagerProvider.getEntityQuery("Contacts"), true, true);
+        let item = null;
+        var numberInput = null;
+        this.items.currentChanged.addHandler(() => {
+            item = this.items.currentItem;
+        });
+        this.items.collectionChanged.addHandler(() => {
+            item = this.items.currentItem;
+        });
+        this.items.querySucceeded.addHandler((sender, e) => {
+            if (numberInput) {
+                numberInput.max = this.items.pageCount;
+            }
+            this.entityManagerProvider.querySucceeded(e.data);
+        });
+        this.items.queryFailed.addHandler((sender, e) => {
+            this.entityManagerProvider.queryFailed(e.data.message);
+        });
+        this.items.saveSucceeded.addHandler((sender, e) => {
+            this.entityManagerProvider.saveSucceeded(e.data);
+        });
+        this.items.saveFailed.addHandler((sender, e) => {
+            this.entityManagerProvider.saveFailed(e.data, this.entityManager);
+        });
     }
 }
 //# sourceMappingURL=welcome.js.map
--- FlexGrid/src/welcome.ts
+++ FlexGrid/src/welcome.ts
@@ -1,3 +1,87 @@
+import {autoinject, inject, singleton} from "aurelia-framework";
+import {EntityManagerProvider} from "resources/data/entity-manager-provider";
+import errorHandler from "resources/core/error-handler";
+var wijmo: any;
+
 export class Welcome {
-    heading:string = "Welcome";
+    heading: string = "Welcome";
+
+    location: string = "FileRowMaps";
+    entities: any[] = [];
+
+    entityManager: breeze.EntityManager;
+    constructor(private entityManagerProvider: EntityManagerProvider, private element: HTMLElement) {
+        console.log("mapping-list cstr");
+        console.log(entityManagerProvider.toString());
+        console.log(element.toString());
+        entityManagerProvider.initializeEntityManager()
+            .then((em: breeze.EntityManager) => {
+                this.entityManager = em;
+                this.getFlexData();
+            });
+    }
+
+    async getInitialData() {
+        let query = new breeze.EntityQuery()
+            .from('FileRowMaps');
+
+        try {
+            let queryResult = await this.entityManager.executeQuery(query);
+            this.entities = queryResult.results;
+
+
+        } catch (error) {
+
+            errorHandler.handleError(error, this.location);
+        }
+    }
+
+    attached() {
+        console.log("attached");
+        let flex = new wijmo.grid.FlexGrid(this.element);
+        flex.initialize({
+            itemsSource: this.items
+        });
+
+    }
+
+    items: any;
+
+    getFlexData() {
+
+
+        this.items = new wijmo.data.BreezeCollectionView(
+            this.entityManager, this.entityManagerProvider.getEntityQuery("Contacts"), true, true);
+        let item: any = null;
+        var numberInput: any = null;
+        this.items.currentChanged.addHandler(() => {
+            item = this.items.currentItem;
+        });
+
+        this.items.collectionChanged.addHandler(() => {
+            item = this.items.currentItem;
+        });
+
+        this.items.querySucceeded.addHandler((sender: any, e: any) => {
+            if (numberInput) {
+                numberInput.max = this.items.pageCount;
+            }
+            this.entityManagerProvider.querySucceeded(e.data);
+        });
+
+        this.items.queryFailed.addHandler((sender: any, e: any) => {
+            this.entityManagerProvider.queryFailed(e.data.message);
+        });
+
+        this.items.saveSucceeded.addHandler((sender: any, e: any) => {
+            this.entityManagerProvider.saveSucceeded(e.data);
+        });
+
+        this.items.saveFailed.addHandler((sender: any, e: any) => {
+            this.entityManagerProvider.saveFailed(e.data, this.entityManager);
+        });
+
+
+    }
+
 }
