9249ed1ad7b73a0b62421009909eb34e481c8eb5
gregoryagu <greg.gum@gmail.com>
2015-12-28 15:51:49 -0500

    Updates

--- FlexGrid/src/welcome.ts
+++ FlexGrid/src/welcome.ts
@@ -2,7 +2,7 @@ import {autoinject, inject, singleton} from "aurelia-framework";
 import {EntityManagerProvider} from "resources/data/entity-manager-provider";
 import errorHandler from "resources/core/error-handler";
 import {AppLogger} from "resources/core/app-logger";
-declare var wijmo: any;
+import {BreezeCollectionView} from "resources/data/BreezeCollectionView";
 
 @inject(EntityManagerProvider, AppLogger)
 export class Welcome {
@@ -41,36 +41,36 @@ export class Welcome {
     getFlexData() {
 
 
-        this.items = new wijmo.data.BreezeCollectionView(
+        this.items = new BreezeCollectionView(
             this.entityManager, this.entityManagerProvider.getEntityQuery("Contacts"), true, true);
         let item: any = null;
         var numberInput: any = null;
-        this.items.currentChanged.addHandler(() => {
-            item = this.items.currentItem;
-        });
+        //this.items.currentChanged.addHandler(() => {
+        //    item = this.items.currentItem;
+        //});
 
-        this.items.collectionChanged.addHandler(() => {
-            item = this.items.currentItem;
-        });
+        //this.items.collectionChanged.addHandler(() => {
+        //    item = this.items.currentItem;
+        //});
 
-        this.items.querySucceeded.addHandler((sender: any, e: any) => {
-            if (numberInput) {
-                numberInput.max = this.items.pageCount;
-            }
-            this.entityManagerProvider.querySucceeded(e.data);
-        });
+        //this.items.querySucceeded.addHandler((sender: any, e: any) => {
+        //    if (numberInput) {
+        //        numberInput.max = this.items.pageCount;
+        //    }
+        //    this.entityManagerProvider.querySucceeded(e.data);
+        //});
 
-        this.items.queryFailed.addHandler((sender: any, e: any) => {
-            this.entityManagerProvider.queryFailed(e.data.message);
-        });
+        //this.items.queryFailed.addHandler((sender: any, e: any) => {
+        //    this.entityManagerProvider.queryFailed(e.data.message);
+        //});
 
-        this.items.saveSucceeded.addHandler((sender: any, e: any) => {
-            this.entityManagerProvider.saveSucceeded(e.data);
-        });
+        //this.items.saveSucceeded.addHandler((sender: any, e: any) => {
+        //    this.entityManagerProvider.saveSucceeded(e.data);
+        //});
 
-        this.items.saveFailed.addHandler((sender: any, e: any) => {
-            this.entityManagerProvider.saveFailed(e.data, this.entityManager);
-        });
+        //this.items.saveFailed.addHandler((sender: any, e: any) => {
+        //    this.entityManagerProvider.saveFailed(e.data, this.entityManager);
+        //});
 
 
     }
